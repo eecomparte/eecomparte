56219a26c598ee051c26d3c6e7892a74d302c3b6
Mariana Adrien <mariana.cecilia.adrien.hernandez@everis.com>
2019-12-10 18:21:58 +0100

    .

--- RDA/DES/rutas_modulos_DES.sql
+++
@@ -1,6 +0,0 @@
-
-UPDATE CCR_CONF_MODULE SET MODULE_URL = 'http://es5dx02bsab.bancsabadell.com:8880/ccr/micro/' WHERE ID_MODULE='MICRO';
-UPDATE CCR_CONF_MODULE SET MODULE_URL = 'http://es5dx02bsab.bancsabadell.com:8880/ccr/appccr/' WHERE ID_MODULE='CCR';
-UPDATE CCR_CONF_MODULE SET MODULE_URL = 'http://es5dx02bsab.bancsabadell.com:8880/ccr/rbp/' WHERE ID_MODULE='RBP';
-UPDATE CCR_CONF_MODULE SET MODULE_URL = 'http://es5dx02bsab.bancsabadell.com:8880/ccr/garantias/' WHERE ID_MODULE='GARAN';
-COMMIT;
